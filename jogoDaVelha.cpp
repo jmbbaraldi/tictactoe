5e99c8fe75d0ea34ff17906bc4101cc7aa25da1c
João Baraldi <jmbbaraldi@gmail.com>
2024-03-01 10:47:13 -0300

    Reads the play and changes the turn

--- jogoDaVelha.cpp
+++ jogoDaVelha.cpp
@@ -38,12 +38,37 @@ void jogo() {
     ///Variáveis gerais
     char tabuleiro[3][3];                               //Tabuleiro do jogo
     int linha, coluna;                                  //Auxiliar matriz
+    int linhaJogada, colunaJogada;                      //Jogador posiciona sua jogada
+    int estadoDeJogo = 1;                               //0=sem jogo; 1=com jogo
+    int turnoDoJogador = 1;                             //1 = X; 0 = O;
 
     //Função que inicia/popula o tabuleiro
     iniciaTabuleiro(tabuleiro);
 
-    //Função que exibe o tabuleiro
-    exibeTabuleiro(tabuleiro);
+    while(estadoDeJogo = 1) {
+
+        limpaTela();
+
+        //Função que exibe o tabuleiro
+        exibeTabuleiro(tabuleiro);
+
+        cout << "Jogador, digite uma linha: ";
+        cin >> linhaJogada;
+        cout << "Jogador, digite uma coluna: ";
+        cin >> colunaJogada;
+
+        if(turnoDoJogador == 1) {                       //Jogador do X
+            tabuleiro[linhaJogada][colunaJogada] = 'X';
+
+            turnoDoJogador = 2;
+        }
+        else {                                          //Jogador do O
+            tabuleiro[linhaJogada][colunaJogada] = 'O';
+
+            turnoDoJogador = 1;
+        }
+
+    }
 }
 
 void menuInicial() {
